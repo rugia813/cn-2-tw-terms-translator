d7b991e2e781378219587e326708780fa220728a
rugia <rugia813@yahoo.com.tw>
2020-11-07 23:04:45 +0800

    refactor

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function translate() {
-    chrome.tabs.query({ active: true,currentWindow:true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { "message": "translate" });  
     });
 }
