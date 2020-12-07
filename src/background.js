9bc9a7645dcb5ae92ae3a6315d98b55b77730d2d
Janway <Janway.Li@goldnet.com>
2020-12-07 14:42:16 +0800

    project structure adjust

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
 function translate() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { "message": "translate" });  
+        chrome.tabs.sendMessage(tabs[0].id, { "message": "translate" });
     });
 }
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
-        "title": "翻譯中國軟工技術用語",
+        "title": "翻譯中國IT用語",
         "contexts": ['all'],
         "onclick": translate,
     });
